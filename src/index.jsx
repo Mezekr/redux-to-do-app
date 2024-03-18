d06e5e9e988f91ac84e8642fbcba195a6b5bd150
Mezekr W <66778493+Mezekr@users.noreply.github.com>
2024-03-18 11:45:56 +0000

    rename redux modules

--- src/index.jsx
+++ src/index.jsx
@@ -1,12 +1,1 @@
-import { createRoot } from "react-dom/client";
-import MainView from "./components/main-view/main-view";
 
-import "./index.scss";
-
-function App() {
-  return <MainView />;
-}
-
-const rootElement = document.getElementById("root");
-const root = createRoot(rootElement);
-root.render(<App />);
