aec507f5b6017352841555f44d2fa501d207c1cd
Mezekr W. <66778493+Mezekr@users.noreply.github.com>
2024-03-19 10:14:21 +0000

    check store intial state

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import MainView from "./components/main-view/main-view";
 import store from "./store/store";
+import { addTodo } from "./actions/actions";
+
+console.log("Intial State: ", store.getState());
 
 function App() {
-  console.log(store);
   return <MainView />;
 }
 
