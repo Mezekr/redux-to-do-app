85c42a5eaad3cdf9803bc7371aff090e3c09b3af
Mezekr W. <66778493+Mezekr@users.noreply.github.com>
2024-03-19 10:18:20 +0000

    dispatch first addTodo action

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import store from "./store/store";
 import { addTodo } from "./actions/actions";
 
 console.log("Intial State: ", store.getState());
+store.dispatch(addTodo("Dispatch my first action"));
+console.log("State after dispatch: ", store.getState());
 
 function App() {
   return <MainView />;
